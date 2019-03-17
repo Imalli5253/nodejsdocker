4ff657b9318bc29e6802ad5ab2ba6ac28fb3ca36
malli86271 <pnr.759@gmail.com>
2019-03-17 18:11:49 +0530

    Docker Publish groovy script Added

---
+++ jobs-dsl/nodejsdocker.groovy
@@ -0,0 +1,22 @@
+job{
+    scm{
+        git('https://github.com/Imalli5253/nodejsdocker.git')
+    }
+    triggers{
+     scm('H/5 * * * *')
+    }
+    wrappers{
+        nodejs('nodejs')
+    }
+ step{
+       dockerBuildAndPublish {
+            repositoryName('nagamalli/dockerpublish')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+ }   
+}
